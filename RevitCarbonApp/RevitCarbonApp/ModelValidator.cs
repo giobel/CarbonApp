def066c48352a437444f1cf2e3fa2371abddee5f
Bonesy28 <j_washbourne@hotmail.com>
2022-04-09 22:00:14 +0100

    putting steps in to the Model

--- RevitCarbonApp/RevitCarbonApp/ModelValidator.cs
+++ RevitCarbonApp/RevitCarbonApp/ModelValidator.cs
@@ -8,6 +8,16 @@ namespace RevitCarbonApp
 {
     internal class ModelValidator
     {
-        //initial push as test
+        //get all elements in project
+
+        //iterate for objects to make sure carbon coefficient is not null
+
+        // if null add object to list
+
+        //if list != null of empty create new datagrid form with object
+
+        //populate objects with coefficient and push objects back to the cs code behind.
+
+        //commit changes to Revit
     }
 }
